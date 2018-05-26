c83d65e55c77f685be9ea300e01518f7dfe29763
jjorge <jjorge@dsic.upv.es>
2018-05-27 00:56:44 +0200

    New script with env.vars

---
+++ templates/run.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+echo "hi"
+export HADOOP_HOME=/opt/hadoop
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export PATH=$PATH:$HADOOP_HOME/bin:$SPARK_HOME/bin
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export HADOOP_HDFS_HOME=/opt/hadoop-2.9.0
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${JAVA_HOME}/jre/lib/amd64/server
+export CLASSPATH=$(${HADOOP_HDFS_HOME}/bin/hadoop classpath --glob)
+
+python3 distributed_example_adv.py
