52a3876376e170430b2e494d77f1ab06285af76f
jjorge <jjorge@dsic.upv.es>
2018-05-31 20:46:04 +0200

    Two different scripts to launch the different training schemes

--- templates/run.sh
+++ templates/run.sh
@@ -1,5 +1,6 @@
 #/bin/bash
-echo "hi"
+echo "Hello world!"
+
 export HADOOP_HOME=/opt/hadoop
 export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
 export PATH=$PATH:$HADOOP_HOME/bin:$SPARK_HOME/bin
@@ -8,5 +9,4 @@ export HADOOP_HDFS_HOME=/opt/hadoop-2.9.0
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${JAVA_HOME}/jre/lib/amd64/server
 export CLASSPATH=$(${HADOOP_HDFS_HOME}/bin/hadoop classpath --glob)
 
-#python3 distributed_example_adv.py
-python3 distributed_example_s3simple.py
+python3 distributed_example_adv.py
---
+++ templates/run_s3.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+echo "Hello world!"
+
+python3 distributed_example_s3simple.py
